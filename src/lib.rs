62e364668c474c62262af73eb9eeb691a71271f9
Romain Desarzens <hib.pub@protonmail.com>
2021-01-10 16:02:33 +0100

    Section 12.5

--- src/lib.rs
+++ src/lib.rs
@@ -21,7 +21,38 @@ impl Config {
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let content = fs::read_to_string(config.filename)?;
 
-    println!("With text\n{}", content);
+    for line in search(&config.query, &content) {
+        println!("{}", line);
+    }
 
     Ok(())
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str>
+{
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
 }
